b02b3ab388ed4ebca2d9356ea8e222b301d57a6a
Fernando García <148652897+FerGaram@users.noreply.github.com>
2025-06-11 16:32:48 -0600

    Back: ejemplo entrada, Front: lienzo interactivo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,56 @@
-import { useState } from 'react'
-import { ReactFlow, Controls, Background } from '@xyflow/react'
+import { useState, useCallback } from 'react'
+import { ReactFlow, Controls, Background, applyEdgeChanges, applyNodeChanges, type NodeChange, type EdgeChange, addEdge } from '@xyflow/react'
 import '@xyflow/react/dist/style.css'
 import './App.css'
 
-const nodes = [
+const nodosIniciales = [
   {
     id: '1',
     position: { x: 0, y: 0 },
-    data: { label: 'Prueba' }
+    data: { label: 'Primer nodo' }
+  },
+  {
+    id: '2',
+    position: { x: 100, y: 100 },
+    data: { label: 'Segundo nodo' }
+  }
+]
+
+const aristasIniciales = [
+  {
+    id: 'e1-2', source: '1', target: '2', label: 'Arista'
   }
 ]
 
 function Flow() {
-  // const [count, setCount] = useState(0)
+  const [nodes, setNodes] = useState(nodosIniciales)
+  const [edges, setEdges] = useState(aristasIniciales)
+
+  const onNodesChange = useCallback(
+    (changes: NodeChange<{ id: string; position: { x: number; y: number }; data: { label: string } }>[]) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [],
+  )
+
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange<{ id: string; source: string; target: string; label: string }>[]) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [],
+  )
+
+  const onConnect = useCallback(
+    (params: any) => setEdges((eds) => addEdge(params, eds)),
+    [],
+  )
 
   return (
     <div style={{ height: '100%' }}>
-      <ReactFlow nodes={nodes}>
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
         <Background />
         <Controls />
       </ReactFlow>
