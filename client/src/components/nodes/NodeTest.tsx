cdeb090efda5e1e0429144feae53fd629f46f3c6
Wilmer Hernández Herrera <93896257+Amiguito2110@users.noreply.github.com>
2025-06-15 16:08:18 -0600

    Coordenadas en los Nodos #1
    Se agregaron las coordenadas de los nodos fuera de ellos al "Cuadrado"

--- client/src/components/nodes/NodeTest.tsx
+++ client/src/components/nodes/NodeTest.tsx
@@ -1,16 +1,22 @@
 import { Handle, Position, type BuiltInNode, type NodeProps } from "@xyflow/react"
 
 // Nodo con cuatro conectores a todos sus lados, y con un label para su contenido
-export const NodeTest = ({ data }: NodeProps<BuiltInNode>) => {
+export const NodeTest = ({ data, positionAbsoluteX, positionAbsoluteY}: NodeProps<BuiltInNode>) => {
     return (
         <div className="node-test">
             <Handle type="source" position={Position.Top} id="top" />
             <Handle type="source" position={Position.Left} id="left" />
-            <label htmlFor="text">{data?.label}</label>
+
+            <div className="label-container">
+                <label htmlFor="text">{data?.label}</label>
+                <div className="node-coords">
+                    ({Math.round(positionAbsoluteX)}, {Math.round(positionAbsoluteY)})
+                </div>
+            </div>
+
             <Handle type="source" position={Position.Bottom} id="bottom" />
             <Handle type="source" position={Position.Right} id="right" />
         </div>
+
     )
 }
-
-// Para darle estilo pueden utilizar el archivo index.css, ahí también encontrarán el ejemplo de cómo se hizo con la clase node-test
